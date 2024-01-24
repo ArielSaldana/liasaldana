28d3c02a1abe6325eb423d39e13228d7978bb2b6
Ariel Saldana <ariel@unreal.io>
2024-01-24 18:24:32 -0500

    Update progress

--- src/pages/Unreal.tsx
+++ src/pages/Unreal.tsx
@@ -4,6 +4,7 @@ import Styles from "./Pages.module.scss";
 import './Pages.module.scss'
 import CaseDescription from "../components/case-description/CaseDescription.tsx";
 import LargeImageDisplay from "../components/large-image-display/LargeImageDisplay.tsx";
+import unrealImage from "@/assets/unreal_wallet_preview.jpeg";
 
 export default function Unreal() {
     return (
@@ -12,11 +13,12 @@ export default function Unreal() {
             <div className={Styles.pageContentWrapper}>
                 <BreadcrumbMenu/>
                 <main>
-                    <CaseDescription title={"Unreal Wallet"} description={"Unreal Wallet is a comprehensive cryptocurrency wallet app that offers users the convenience of tracking their entire portfolio, monitoring market trends, and engaging with other investors, all within a single intuitive interface"}/>
-                    <LargeImageDisplay/>
-                    <CaseDescription title={"Quick Scope"} description={"This case study covers the UI of a crypto currency app. The basis of the app is to integrate popular features from other apps seamlessly into one. \n" +
-                        "\n" +
-                        "I was the sole designer and worked alongside the client and engineer for 2 months and delivered mood boards, wire-frames, and low & high fidelity mock-ups."}/>
+                    <CaseDescription title={"Unreal Wallet"}
+                                     description={["Unreal Wallet is a comprehensive cryptocurrency wallet app that offers users the convenience of tracking their entire portfolio, monitoring market trends, and engaging with other investors, all within a single intuitive interface"]}/>
+                    <LargeImageDisplay image={unrealImage} altText={"Unreal Wallet"}/>
+                    <CaseDescription title={"Quick Scope"}
+                                     description={["This case study covers the UI of a crypto currency app. The basis of the app is to integrate popular features from other apps seamlessly into one.",
+                                         "I was the sole designer and worked alongside the client and engineer for 2 months and delivered mood boards, wire-frames, and low & high fidelity mock-ups."]}/>
 
                 </main>
             </div>
